7f15eb7e2ae157f22df345dadc6db931d6f19b77
Engr. Max Angelo Perin <perinmaks@outlook.ph>
2020-03-10 08:38:29 +0800

    Renamed the name of database.

--- DATABASE/softeng_exam.sql
+++ DATABASE/softeng_exam.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v9.62 
-MySQL - 5.5.54 : Database - exam generator
+MySQL - 5.5.54 : Database - softeng_exam
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 5.5.54 : Database - exam generator
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`exam generator` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`softeng_exam` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `exam generator`;
+USE `softeng_exam`;
 
 /*Table structure for table `answer key table` */
 
